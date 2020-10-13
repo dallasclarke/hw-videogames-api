b7d203c60433e2ce7b1244c7a28afed46666b3dd
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-10-13 12:43:30 -0700

    Path create game started. Testing now

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -5,6 +5,31 @@ const {getAllGames, getSingleGame} = require('../controllers/userControllers');
 
 router.get('/all-games', getAllGames);
 router.get('/single-game/:id', getSingleGame);
+router.post('/create-game', (req, res) => {
+    if (!req.body.name || !req.body.description || !req.body.yearReleased || !req.body.playTime) {
+        return res
+        .status(400)
+        .json({confirmation: 'fail', message: 'All inputs must be filled in'});
+    }
+    let existingGame = games.filter((game) => game.name === req.body.email);
+    if (existingGame.length) {
+        return res
+        .status(400)
+        .send('This game already exists.')
+    }
+
+    let newGame = {};
+
+    newGame.name = req.body.name;
+    newGame.description = req.body.description;
+    newGame.yearReleased = req.body.yearReleased;
+    newGame.playTime = req.body.playTime;
+    newGame.id = String(users.length + 1);
+
+    games.push(newGame);
+    
+    return res.status(200).json({confirmation: 'Success', newGame});
+})
 
 
 
